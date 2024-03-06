30318c626a6e61f8db2db6a952c2d6c82bb95c83
Akash Ashok <akashask2012@gmail.com>
2024-03-06 15:21:49 +0530

    corheaders

--- Parking_Space_Finder/settings.py
+++ Parking_Space_Finder/settings.py
@@ -41,11 +41,13 @@ INSTALLED_APPS = [
     'api',
     'rest_framework.authtoken',
     'drf_yasg',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -140,3 +142,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS=True
